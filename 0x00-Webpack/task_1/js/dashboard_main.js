1ba5493c06ba80611493512d8082f1b8b470dd80
Daniel Enagu <daniel@cygnusspin.com>
2023-07-24 23:08:05 +0100

    Updates to entry point

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -4,15 +4,15 @@ import _ from 'lodash';
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
-$('body').append('<p id="count"></p>');
+$('body').append("<p id='count'></p>");
 $('body').append('<p>Copyright - Holberton School</p>');
 
-let counter = 0;
+let count = 0;
 
 function updateCounter() {
-  counter++;
+  count++;
 
-  $('#count').html(`${counter} clicks on the button`);
+  $('#count').html(`${count} clicks on the button`);
 }
 
 $('button').on('click', _.debounce(updateCounter, 500));
