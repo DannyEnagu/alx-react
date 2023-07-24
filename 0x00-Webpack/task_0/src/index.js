b75aa00ee4af847e272243fa4b880300990ea235
Daniel Enagu <daniel@cygnusspin.com>
2023-07-24 22:11:05 +0100

    Updates to src

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,15 +1,5 @@
 import $ from 'jquery';
 
-$(function () {
-  // Paragraph 1
-  const paragraph1 = $('<p>').text('Holberton Dashboard');
-  $('body').append(paragraph1);
-
-  // Paragraph 2
-  const paragraph2 = $('<p>').text('Dashboard data for the students');
-  $('body').append(paragraph2);
-
-  // Paragraph 3
-  const paragraph3 = $('<p>').text('Copyright - Holberton School');
-  $('body').append(paragraph3);
-});
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
