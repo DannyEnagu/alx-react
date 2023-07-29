605219c65b2ace9883ce25be506d7499d39a058e
Daniel Enagu <daniel@cygnusspin.com>
2023-07-29 18:15:08 +0100

    Task 5 complete and fixed

--- 0x01-react_intro/task_5/dashboard/src/Notifications/Notifications.test.js
+++ 0x01-react_intro/task_5/dashboard/src/Notifications/Notifications.test.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import { swallow } from 'enzyme';
-import Notifications from './Notifications';
+import { shallow } from 'enzyme';
 
+import Notifications from './Notifications';
 
 describe('Notification component tests', () => {
   it('Test that Notifications renders without crashing', () => {
-    const notifications = swallow(<Notifications />);
+    const notifications = shallow(<Notifications />);
     expect(notifications).toBeDefined();
   });
 
   it('Test that Notifications renders three list items', () =>{
-    const notifications = swallow(<Notifications />);
-    expect(notifications.find('li')).to.have.lengthOf(3);
+    const notifications = shallow(<Notifications />);
+    expect(notifications.find('li')).toHaveLength(3);
   });
 
   it('Test Notifications renders the text `Here is the list of notifications`', () => {
-    const notifications = swallow(<Notifications />);
+    const notifications = shallow(<Notifications />);
     expect(notifications.find('p').text())
       .toBe('Here is the list of notifications');
   });
