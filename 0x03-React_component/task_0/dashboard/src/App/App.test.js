6ba60b2c92b9b044acb5d8ab83b44cf705aae38b
Daniel Enagu <daniel@cygnusspin.com>
2023-08-02 09:40:51 +0100

    Add more tests

--- 0x03-React_component/task_0/dashboard/src/App/App.test.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.test.js
@@ -4,15 +4,28 @@ import { shallow } from 'enzyme';
 import App from './App';
 import Notifications from '../Notifications/Notifications';
 import Header from '../Header/Header';
-import Login from '../Login/Login';
 import Footer from '../Footer/Footer';
+import Login from '../Login/Login';
 import CourseList from '../CourseList/CourseList';
 
 describe('App Component Test', () => {
-  // const isLoggedIn = true;
+  const isLoggedIn = true;
   const app = shallow(<App />);
   it('Test that App renders without crashing', () => {
     expect(app).toBeDefined();
+  });
+
+  it('renders different 4 child components', () => {
+    expect(app.containsAllMatchingElements([
+      // <Notifications listNotifications={[]} />,
+      <Header />,
+      <Login />,
+      <Footer />
+    ])).toEqual(true);
+  });
+
+  it('Verify that <Notifications /> is in App', () => {
+    expect(app.find(Notifications)).toHaveLength(1);
   });
 
   it('check that CourseList is not displayed', () => {
