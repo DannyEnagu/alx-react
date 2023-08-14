7952957fc3e248935f256e24d7c6968752744b3b
Daniel Enagu <daniel@cygnusspin.com>
2023-08-14 20:57:52 +0100

    Task 3. Context consumer & advanced state

--- 0x05-react_state/task_2/dashboard/src/Login/Login.test.js
+++ 0x05-react_state/task_2/dashboard/src/Login/Login.test.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 
 import Login from './Login';
+import { StyleSheetTestUtils } from 'aphrodite';
 
 describe('<Login /> component tests', () => {
-  const wrapper = shallow(<Login />);
+  let wrapper;
+
+  beforeEach(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
+    wrapper = mount(<Login />);
+  });
 
   it('renders without crashing', () => {
     expect(wrapper).toBeDefined();
@@ -16,5 +22,15 @@ describe('<Login /> component tests', () => {
 
   it('renders 2 input tags', () => {
     expect(wrapper.find('label')).toHaveLength(2);
+  });
+
+  it('verify that the submit button is disabled by default', () => {
+    expect(wrapper.find("input[type='submit']").props().disabled).toEqual(true);
+  });
+
+  it('verify that after changing the value of the two inputs, the button is enabled', () => {
+    wrapper.find('#email').simulate('change', {target: {value: 't'}});
+    wrapper.find('#password').simulate('change', {target: {value: 't'}});
+    expect(wrapper.find("input[type='submit']").props().disabled).toEqual(false);
   });
 });
