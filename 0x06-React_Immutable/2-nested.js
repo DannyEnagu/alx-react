3d488d2089dad7d78634bd1d8bbfa2b958edaf66
Daniel Enagu <daniel@cygnusspin.com>
2023-08-16 12:28:04 +0100

    Add shebang to beginning of file

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,7 +1,9 @@
-const { Map } = require('immutable');
+#!/usr/bin/node
+
+const { fromJS } = require('immutable');
 
 // Accessing nested elements
-export default function accessImmutableObject (object, array) {
-  const nested = Map(object);
+export default function accessImmutableObject(object, array) {
+  const nested = fromJS(object);
   return nested.getIn(array);
 }
