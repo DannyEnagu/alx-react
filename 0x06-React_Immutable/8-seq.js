5fc927da64c232af3c553bfc66f3d519092349d2
Daniel Enagu <daniel@cygnusspin.com>
2023-08-16 19:00:06 +0100

    Add updates to task 8

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -8,8 +8,8 @@ export default function printBestStudents(object) {
   const JSObj = filteredStudents.toJS();
 
   Object.keys(JSObj).map((key) => {
-    JSObj[key].firstName = JSObj[key].firstName.charAt(0).toUpperCase() + JSObj[key].firstName;
-    JSObj[key].lastName = JSObj[key].lastName.charAt(0).toUpperCase() + JSObj[key].lastName;
+    JSObj[key].firstName = JSObj[key].firstName.charAt(0).toUpperCase() + JSObj[key].firstName.slice(1);
+    JSObj[key].lastName = JSObj[key].lastName.charAt(0).toUpperCase() + JSObj[key].lastName.slice(1);
     return JSObj[key];
   });
 
