804b02d2321fbb2001049d5aaed301105030eabb
Daniel Enagu <daniel@cygnusspin.com>
2023-08-22 15:25:50 +0100

    0. Read data from a JSON

---
+++ 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
@@ -0,0 +1,11 @@
+import * as notificationData from '../../notifications.json';
+
+export default function getAllNotificationsByUser (userId) {
+  const filteredData = [];
+  notificationData
+    .forEach((item) => {
+      if (item.author.id === userId) {
+        filteredData.push(item.context);
+      }
+    });
+}
